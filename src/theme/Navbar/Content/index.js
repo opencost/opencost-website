04b9770e12d150468f815818b8e73cf429fcb026
elenalape <hi@elenalape.com>
2022-06-01 19:16:21 +0100

    add star count

--- src/theme/Navbar/Content/index.js
+++ src/theme/Navbar/Content/index.js
@@ -1,33 +1,36 @@
-import React from 'react';
-import NavbarItem from '@theme/NavbarItem';
-import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
-import SearchBar from '@theme/SearchBar';
+import React, { useState, useEffect } from "react";
+import NavbarItem from "@theme/NavbarItem";
+import NavbarColorModeToggle from "@theme/Navbar/ColorModeToggle";
+import SearchBar from "@theme/SearchBar";
 import {
   splitNavbarItems,
   useNavbarMobileSidebar,
   useThemeConfig,
-} from '@docusaurus/theme-common';
-import NavbarMobileSidebarToggle from '@theme/Navbar/MobileSidebar/Toggle';
-import NavbarLogo from '@theme/Navbar/Logo';
-import NavbarSearch from '@theme/Navbar/Search';
-import styles from './styles.module.css';
+} from "@docusaurus/theme-common";
+import NavbarMobileSidebarToggle from "@theme/Navbar/MobileSidebar/Toggle";
+import NavbarLogo from "@theme/Navbar/Logo";
+import NavbarSearch from "@theme/Navbar/Search";
+import styles from "./styles.module.css";
+
+import { AiFillStar } from "react-icons/ai";
 
 function useNavbarItems() {
   // TODO temporary casting until ThemeConfig type is improved
   return useThemeConfig().navbar.items;
 }
 
-function NavbarItems({items}) {
-  return (
+function NavbarItems({ items }) {
+  let result = (
     <>
       {items.map((item, i) => (
         <NavbarItem {...item} key={i} />
       ))}
     </>
   );
+  return result;
 }
 
-function NavbarContentLayout({left, right}) {
+function NavbarContentLayout({ left, right }) {
   return (
     <div className="navbar__inner">
       <div className="navbar__items">{left}</div>
@@ -40,7 +43,18 @@ export default function NavbarContent() {
   const mobileSidebar = useNavbarMobileSidebar();
   const items = useNavbarItems();
   const [leftItems, rightItems] = splitNavbarItems(items);
-  const autoAddSearchBar = !items.some((item) => item.type === 'search');
+  const autoAddSearchBar = !items.some(item => item.type === "search");
+
+  const [stargazers, setStargazers] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/repos/kubecost/cost-model")
+      .then(res => res.json())
+      .then(data => {
+        setStargazers(data.stargazers_count);
+      });
+  }, []);
+
   return (
     <NavbarContentLayout
       left={
@@ -56,12 +70,31 @@ export default function NavbarContent() {
         // Ask the user to add the respective navbar items => more flexible
         <>
           <NavbarItems items={rightItems} />
+
           <NavbarColorModeToggle className={styles.colorModeToggle} />
           {autoAddSearchBar && (
             <NavbarSearch>
               <SearchBar />
             </NavbarSearch>
           )}
+          <a
+            href="https://github.com/kubecost/cost-model"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:no-underline text-yellow-400 hover:text-green-600"
+          >
+            <span className="">
+              {stargazers && (
+                <>
+                  <AiFillStar
+                    size={20}
+                    className="items-center flex-row align-middle mb-1 mr-1"
+                  />
+                  {stargazers}
+                </>
+              )}
+            </span>
+          </a>
         </>
       }
     />
