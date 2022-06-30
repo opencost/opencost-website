74de8b93678bbe8b46887296aa0d736335c68513
Sean Pomeroy <spom@kubecost.com>
2022-06-30 14:27:09 -0400

    Add CNCF section and LF Footer (#30)
    * add standard LF footer
    
    * add CNCF sandbox section
    
    * adjust padding and link hover
    
    * left align LF footer

---
+++ src/components/CNCF.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function CNCF() {
+  return (
+    <div className="flex flex-col gap-y-3 px-10 py-10 md:py-10 mt-10 md:mt-10 mb-10 md:mb-10">
+      <div className="flex flex-col items-center">
+        <img className="w-1/4 h-auto" src="/img/cncf-color.png" alt="CNCF Foundation" />
+      </div>
+      <div className="text-lg text-center flex justify-center font-anek mt-4 max-w-5xl mx-auto leading-relaxed">
+        OpenCost is a&nbsp;
+        <a
+          href="https://www.cncf.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-green-300 hover:no-underline"
+        >
+          Cloud Native Computing Foundation
+        </a>
+        &nbsp;sandbox project.
+      </div>
+    </div>
+  );
+}
