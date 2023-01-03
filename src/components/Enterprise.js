2eb75af4911553491ed03a7e702bcb147a43c892
Burke Shartsis <burke@burkeshartsis.com>
2023-01-03 15:47:04 -0500

    update enterprise hero to match style guide
    - also makes this component more reusable by moving some "page specific" layout positioning outside of the component.
    
    TODO:  This page layout padding that was moved out could be consolidated better and resused in more places

--- src/components/Enterprise.js
+++ src/components/Enterprise.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { FaGithub } from "react-icons/fa";
 
-export default function Enterprise() {
+export default function Enterprise({ className }) {
   return (
-    <section className="pt-20 flex flex-col-reverse xl:flex-row items-center justify-center px-10 sm:px-20 xl:px-40">
-      <hgroup className="xl:w-2/3">
+    <section className={`flex flex-col-reverse md:flex-row items-end ${className}`}>
+      <hgroup className="px-8">
         <h1 className="text-4xl sm:text-5xl font-anek font-normal pb-6">
           Making cloud costs visible with open source & enterprise solutions
         </h1>
-        <p>
+        <p className="text-2xl leading-11.5">
           OpenCost is an open source, CNCF project developed to help teams manage Kubernetes cloud
           costs in an environment that embraces community collaboration. With unified cost
           monitoring, customized alerts and so much more, we are on a mission to simplify cost
@@ -16,7 +15,11 @@ export default function Enterprise() {
         </p>
       </hgroup>
 
-      <img className="w-full lg:w-5/12" src="/img/enterprise-stack.png" alt="Enterprise Stacks" />
+      <img
+        className="w-full md:w-1/3 max-w-min"
+        src="/img/enterprise-stack.png"
+        alt="Enterprise Stacks"
+      />
     </section>
   );
 }
