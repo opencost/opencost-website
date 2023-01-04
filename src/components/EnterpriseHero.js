1f8f7329b1e4bc0fc82f336f7e6a2d8cb6a3b11c
Burke Shartsis <burke@burkeshartsis.com>
2023-01-04 17:52:16 -0500

    make `EnterpriseHero` more fluid
    - uses an svg for the image
    - lots of very specific css to get evertything to line up just right

--- src/components/EnterpriseHero.js
+++ src/components/EnterpriseHero.js
@@ -1,9 +1,10 @@
 import React from "react";
+import EnterpriseStacksImage from "/img/enterprise-stack.svg";
 
 export default function EnterpriseHero({ className }) {
   return (
-    <section className={`flex flex-col-reverse md:flex-row items-end md:items-center ${className}`}>
-      <hgroup className="px-8">
+    <section className={`flex flex-col-reverse lg:flex-row items-end lg:items-center ${className}`}>
+      <hgroup className="px-8 basis-3/5">
         <h1 className="text-4xl sm:text-5xl font-anek font-normal pb-6">
           Making cloud costs visible with open source & enterprise solutions
         </h1>
@@ -15,11 +16,11 @@ export default function EnterpriseHero({ className }) {
         </p>
       </hgroup>
 
-      <img
-        className="w-full md:w-1/3 max-w-min"
-        src="/img/enterprise-stack.png"
-        alt="Enterprise Stacks"
-      />
+      <figure className="pb-10 lg:pb-0 m-0 w-2/3 lg:w-auto flex grow items-end">
+        <EnterpriseStacksImage className="sm:shrink-0 w-full max-w-[455px]" />
+
+        <span className="-ml-1 mb-[46px] h-[5px] grow bg-[#231F20] hidden sm:block" />
+      </figure>
     </section>
   );
 }
