8aa845d5b07357147a52c5894866eb47c3e5750a
Burke Shartsis <burke@burkeshartsis.com>
2023-01-05 10:30:04 -0500

    add text hover effect to `Kubecost` component "cards"
    I'm not really in love with this how this looks

--- src/components/Kubecost.js
+++ src/components/Kubecost.js
@@ -4,7 +4,7 @@ import KubecostEnterprise from "/img/kubecost/kubecost-enterprise.svg";
 export default function Kubecost() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-2 gap-4">
-      <div className="px-8 py-4 sm:col-span-2 row-span-2 rounded-xl border border-solid border-gray-200 dark:border-gray-800">
+      <div className="px-8 py-4 sm:col-span-2 row-span-2 rounded-xl border border-solid border-gray-200 dark:border-gray-800 group/card">
         <h3 className="my-4">
           <KubecostEnterprise className="max-w-xs" />
           <span className="sr-only">Kubecost Enterprise</span>
@@ -45,26 +45,35 @@ export default function Kubecost() {
           </figure>
         </div>
 
-        <a href="https://www.kubecost.com" className="underline block pt-8">
+        <a
+          href="https://www.kubecost.com"
+          className="underline block pt-8 text-black group-hover/card:text-green-500 group-hover/card:font-black"
+        >
           Learn more about Kubecost’s mission to improve cost visibility
         </a>
       </div>
 
-      <div className="px-8 py-4 rounded-xl border border-solid border-gray-200 dark:border-gray-800">
+      <div className="px-8 py-4 rounded-xl border border-solid border-gray-200 dark:border-gray-800 group/card">
         <h4 className="text-3xl sm:text-2xl font-anek font-normal leading-snug">Getting Started</h4>
         <p>
           Kubecost’s documentation helps to guide onboarding. It’s full of tips, tricks, and useful
           features you may have missed.
         </p>
-        <a href="https://docs.kubecost.com" className="underline">
+        <a
+          href="https://docs.kubecost.com"
+          className="underline text-black group-hover/card:text-green-500 group-hover/card:font-black"
+        >
           Check it out
         </a>
       </div>
 
-      <div className="px-8 py-4 rounded-xl border border-solid border-gray-200 dark:border-gray-800">
+      <div className="px-8 py-4 rounded-xl border border-solid border-gray-200 dark:border-gray-800 group/card">
         <h4 className="text-3xl sm:text-2xl font-anek font-normal leading-snug">Tutorials</h4>
         <p>Read Kubecost’s Blog for in-depth tutorials on feature implementations.</p>
-        <a href="https://blog.kubecost.com" className="underline">
+        <a
+          href="https://blog.kubecost.com"
+          className="underline text-black group-hover/card:text-green-500 group-hover/card:font-black"
+        >
           Check it out
         </a>
       </div>
