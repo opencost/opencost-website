16664b0caa4f929598669f37ce34971672c26979
Matt Ray <github@mattray.dev>
2023-07-18 15:54:01 +1000

    Fixed broken link
    Signed-off-by: Matt Ray <github@mattray.dev>

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -14,7 +14,7 @@ export default function Hero() {
           Kubernetes experts and supported by Kubernetes practitioners, OpenCost
           shines a light into the black box of Kubernetes spend.
         </p>
-        <a href="/docs/install" className="hover:no-underline">
+        <a href="/docs/installation/install" className="hover:no-underline">
           <div className="text-white w-fit py-2 px-4 rounded-md bg-green-400 hover:bg-green-700 transition-colors duration-200">
             <span>Get started</span>
           </div>
