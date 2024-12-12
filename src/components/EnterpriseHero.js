c85e63b1c26db9c574198eec6c7e8ac91dbaea2a
Alex Meijer <ameijer@kubecost.com>
2024-12-12 09:18:18 -0500

    add list items
    Signed-off-by: Alex Meijer <ameijer@kubecost.com>

--- src/components/EnterpriseHero.js
+++ src/components/EnterpriseHero.js
@@ -21,14 +21,34 @@ export default function EnterpriseHero({ className }) {
           OpenCost Enterprise Vendor, organizations must meet the following criteria:
 
           <ul>
-              <li>List item one</li>
-              <li>List item two with subitems:
+              <li>Demonstrate a commitment to OpenCost’s maintenance and improvement
                   <ul>
-                      <li>Subitem 1</li>
-                      <li>Subitem 2</li>
+                      <li>On average, has pull requests accepted monthly into OpenCost or its child projects for features and/or improvements</li>
+                      <li>Help triage and comment on issues anywhere in the OpenCost ecosystem at least 5 times per month</li>
                   </ul>
               </li>
-              <li>Final list item</li>
+
+              <li>Demonstrate an area of expertise in the OpenCost ecosystem
+                  <ul>
+                      <li>Example: Employing one or more OpenCost maintainers</li>
+                      <li>Example: Owning a specific high impact feature or process (testing system, release planning, etc)</li>
+                  </ul>
+              </li>
+
+              <li>Participate in OpenCost’s governance
+                  <ul>
+                      <li>At least 2 members of the vendor’s team attend the community meetings at least 50% of the time</li>
+                  </ul>
+              </li>
+
+              <li>Are OpenCost/FinOps thought leaders
+                  <ul>
+                      <li>Example: Sponsoring staff for the OpenCost booth at CNCF events</li>
+                      <li>Example: Giving talks related to OpenCost or its ecosystem</li>
+                  </ul>
+              </li>
+
+              <li>Provide enterprise level support and functionality for OpenCost or products based directly on it</li>
           </ul>
 
         </p>
